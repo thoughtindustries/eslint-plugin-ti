c36d1059c5b05d00ba577a272cc45da8982770f4
John Webb <john.webb@thoughtindustries.com>
2022-12-06 14:57:47 +0000

    Fixed bug where wrong variable was checked in the while statement

--- tests/lib/rules/id-helper.js
+++ tests/lib/rules/id-helper.js
@@ -57,5 +57,30 @@ ruleTester.run('id-helper', rule, {
       options,
       errors: [{ messageId: 'useIdHelper' }]
     },
+    {
+      code: `const upsertBackgroundJob = (db, jobs) => {
+        return wrapper({
+          rethinkdb: async () => {
+            const r = db.rethinkdb;
+      
+            return (
+              await r.pool.run(
+                r.table('backgroundJobs').insert(jobs, { returnChanges: 'always', conflict: 'replace' })
+              )
+            ).changes.map(change => change.new_val);
+          },
+      
+          postgres: async () => {
+            const result = await postgresBase.getModel(db.postgres, 'backgroundJobs').bulkCreate(jobs, {
+              updateOnDuplicate: ['company']
+            });
+      
+            return toPlainObject(result);
+          }
+        });
+      }`,
+      options,
+      errors: [{ messageId: 'useIdHelper' }]
+    }
   ]
 });

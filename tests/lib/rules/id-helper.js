dd30a7ccc707df38239c1cc3cb0874a9de3f5334
John Webb <jtwebb@users.noreply.github.com>
2022-12-07 08:34:41 -0600

    Check for use of idHelper (#19)
    * Check for use of idHelper
    
    * Remove `insertAndReturnFirstRow` since it's already wrapped.
    
    * Update message
    
    * Fixed bug where wrong variable was checked in the while statement
    
    Co-authored-by: John Webb <john.webb@thoughtindustries.com>

---
+++ tests/lib/rules/id-helper.js
@@ -0,0 +1,86 @@
+const rule = require('../../../lib/rules/id-helper');
+const RuleTester = require('eslint').RuleTester;
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2021 } });
+const options = [{}];
+
+ruleTester.run('id-helper', rule, {
+  valid: [
+    {
+      code: `function upsertBackgroundJob(db, jobs) {
+        const newData = idHelper(data);
+        return wrapper({
+          rethinkdb: async () => {
+            const r = db.rethinkdb;
+      
+            return (
+              await r.pool.run(
+                r.table('backgroundJobs').insert(jobs, { returnChanges: 'always', conflict: 'replace' })
+              )
+            ).changes.map(change => change.new_val);
+          },
+      
+          postgres: async () => {
+            const result = await postgresBase.getModel(db.postgres, 'backgroundJobs').bulkCreate(jobs, {
+              updateOnDuplicate: ['company']
+            });
+      
+            return toPlainObject(result);
+          }
+        });
+      }`,
+      options
+    }
+  ],
+  invalid: [
+    {
+      code: `function upsertBackgroundJob(db, jobs) {
+        return wrapper({
+          rethinkdb: async () => {
+            const r = db.rethinkdb;
+      
+            return (
+              await r.pool.run(
+                r.table('backgroundJobs').insert(jobs, { returnChanges: 'always', conflict: 'replace' })
+              )
+            ).changes.map(change => change.new_val);
+          },
+      
+          postgres: async () => {
+            const result = await postgresBase.getModel(db.postgres, 'backgroundJobs').bulkCreate(jobs, {
+              updateOnDuplicate: ['company']
+            });
+      
+            return toPlainObject(result);
+          }
+        });
+      }`,
+      options,
+      errors: [{ messageId: 'useIdHelper' }]
+    },
+    {
+      code: `const upsertBackgroundJob = (db, jobs) => {
+        return wrapper({
+          rethinkdb: async () => {
+            const r = db.rethinkdb;
+      
+            return (
+              await r.pool.run(
+                r.table('backgroundJobs').insert(jobs, { returnChanges: 'always', conflict: 'replace' })
+              )
+            ).changes.map(change => change.new_val);
+          },
+      
+          postgres: async () => {
+            const result = await postgresBase.getModel(db.postgres, 'backgroundJobs').bulkCreate(jobs, {
+              updateOnDuplicate: ['company']
+            });
+      
+            return toPlainObject(result);
+          }
+        });
+      }`,
+      options,
+      errors: [{ messageId: 'useIdHelper' }]
+    }
+  ]
+});

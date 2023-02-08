c9f429694832eafc1adcf2d46bdfda3c4441f460
John Webb <jtwebb@users.noreply.github.com>
2023-02-08 08:18:29 -0600

    Added new rule that requires 'r' in base repo functions (#28)
    * Added new rule that requires 'r' in base repo functions
    
    * Added in check to make sure it's a repo function
    
    * Allow for this.r as an argument
    
    * DRY'd up a little
    
    ---------
    
    Co-authored-by: John Webb <john.webb@thoughtindustries.com>

---
+++ tests/lib/rules/require-r.js
@@ -0,0 +1,43 @@
+const rule = require('../../../lib/rules/require-r');
+const RuleTester = require('eslint').RuleTester;
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2021 } });
+const options = [{}];
+
+ruleTester.run('require-r', rule, {
+  valid: [
+    {
+      code: `async function getUsers(ids) {
+        const users = await r.users.findById(r, ids);
+      }`,
+      options
+    },
+    {
+      code: `async function getUsers(slug) {
+        const users = await this.r.users.findBySlug(this.r, slug);
+      }`,
+      options
+    },
+    {
+      code: `async function getUsers(slug) {
+        notARepoFunction.insert(slug);
+      }`,
+      options
+    }
+  ],
+  invalid: [
+    {
+      code: `async function getUsers(ids) {
+        const users = await r.users.findById(ids);
+      }`,
+      options,
+      errors: [{ messageId: 'requireR' }]
+    },
+    {
+      code: `async function getUsers(slug) {
+        const users = await this.r.users.findBySlug(slug);
+      }`,
+      options,
+      errors: [{ messageId: 'requireR' }]
+    }
+  ]
+});
